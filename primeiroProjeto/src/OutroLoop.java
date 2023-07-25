723817fe44a00c3d00835f0ccb2f569b97c34660
Leonardo R. Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-07-25 16:49:48 -0300

    Trabalhando com While

--- primeiroProjeto/src/OutroLoop.java
+++ primeiroProjeto/src/OutroLoop.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class OutroLoop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int contadorNotas = 0; 
+
+        while (nota != -1) {
+            System.out.println("Digite a avaliação do seu filme favorito, ou -1 para encerrar: ");
+            nota = leitura.nextDouble();
+            if(nota != -1){
+                mediaAvaliacao += nota;
+                contadorNotas++;
+
+            }
+
+        }
+
+        System.out.println("Média de avaliações " + mediaAvaliacao/contadorNotas);
+    }
     
 }
