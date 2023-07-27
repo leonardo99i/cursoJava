43570b3fc6b500fa900712e3bde34210823e9525
Leonardo R. Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-07-27 12:08:32 -0300

    progredinfdo

--- primeiroProjeto/src/desafioJogoAdvinhacao.java
+++ primeiroProjeto/src/desafioJogoAdvinhacao.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class desafioJogoAdvinhacao {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-        int numeroGerado = new Random().nextInt(100);//Isso ira gerar um numero aleatori entre 0 e 100
+        int numeroGerado = new Random().nextInt(100);//Isso ira gerar um numero aleatorio entre 0 e 100
         int tentativas = 0;
 
-        while(tentativas < 5){
+        while(tentativas < 8){
             System.out.println("Digite um numero entre 0 a 100: ");
             int numeroDigitado = leitura.nextInt();
             tentativas++;
@@ -16,9 +16,9 @@ public class desafioJogoAdvinhacao {
                 System.out.println("Parabens você acertou!!!");
                 break;
             }else if(numeroDigitado < numeroGerado){
-                System.out.println("Numero digitado é menor que o esperado!");   
+                System.out.println("Precisa ser maior");   
             }else{
-                System.out.println("Numero digitado é maior que o esperado");
+                System.out.println("Precisa ser menor");
             }
         }
 
